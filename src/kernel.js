a781d2a60a3c76d38dff3cbd25597c265a8627fb
Dark Hole <dark.hole1@yandex.ru>
2023-03-02 16:24:18 +0300

    Minor fixes

--- src/kernel.js
+++ src/kernel.js
@@ -5,12 +5,12 @@ const { argv, exit } = require('process');
 
 print = (message, callback) => process.stdout.write(message, callback);
 
-if (argv.length < 1) {
-    print("USAGE: node src/kernel.js file.asmX");
+if (argv.length < 3) {
+    print("USAGE: node src/kernel.js pathToFile.asmX\n");
     exit(1);
 }
 
-let fname = argv[1];
+let fname = argv[2];
 let file = fs.readFileSync(fname, { encoding: 'utf8' });
 let parser = Parser.parse(file);
 new Compiler(parser);
