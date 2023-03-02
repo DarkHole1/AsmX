c847ee94d07e9e37280fa42b59ffb763c2c21cfd
Dark Hole <dark.hole1@yandex.ru>
2023-03-02 16:21:29 +0300

    Got rid from compilerx class

--- src/kernel.js
+++ src/kernel.js
@@ -11,15 +11,6 @@ if (argv.length < 1) {
 }
 
 let fname = argv[1];
-new CompilerAsmX({ src: fname });
-
-
-class CompilerAsmX {
-    constructor(config) {
-        this.config = config;
-        this.tokens = [];
-        let file = fs.readFileSync(this.config.src, { encoding: 'utf8' });
-        let parser = Parser.parse(file);
-        new Compiler(parser);
-    }
-}
+let file = fs.readFileSync(fname, { encoding: 'utf8' });
+let parser = Parser.parse(file);
+new Compiler(parser);
